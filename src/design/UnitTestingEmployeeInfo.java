468c503f84d2d012ad6f23f6a752434245f0f1cc
syedfx <63693975+syedfx@users.noreply.github.com>
2022-05-20 21:45:02 -0400

    Design Package Done

--- src/design/UnitTestingEmployeeInfo.java
+++ src/design/UnitTestingEmployeeInfo.java
@@ -1,8 +1,32 @@
 package design;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class UnitTestingEmployeeInfo {
     public static void main(String[] args) {
         //Write Unit Test for all the methods has been implemented in this package.
 
     }
+
+    @Test
+    public void testCalculateEmployeeBonus() {
+        int expectedResult = 12000;
+        int actualResult = EmployeeInfo.calculateEmployeeBonus(120000, "best");
+        Assert.assertEquals( expectedResult, actualResult);
+        System.out.println("test passed");
+    }
+
+    @Test
+    public void testCalculateEmployeePension() throws ParseException {
+        int expectedResult = 12000;
+        int actualResult = EmployeeInfo.calculateEmployeePension(5,1200000);
+        Assert.assertEquals(expectedResult, actualResult);
+        System.out.println("test passed");
+    }
+
 }
